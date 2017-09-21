61a3c5cb1f033c32a17a8e6f56476c4de958c8e7
orbenozio <or.benozio@gmail.com>
2017-09-21 11:47:56 +0300

    Faecbook login

--- Assets/Scripts/Managers/RegistrationManager.cs
+++ Assets/Scripts/Managers/RegistrationManager.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.Data;
+using Facebook.Unity;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using System;
@@ -9,6 +10,12 @@ public delegate void RegistrationDelegte(eRegistrationMethodType reg, bool succe
 
 public class RegistrationManager : QuackMonoBehaviour
 {
+    #region Consts
+
+    private readonly List<string> FAECBOOK_READ_PERMISSIONS = new List<string>() { "public_profile", "email", "user_friends" };
+
+    #endregion
+
     #region Events
 
     public event RegistrationDelegte OnSignInEvent;
@@ -75,6 +82,23 @@ public class RegistrationManager : QuackMonoBehaviour
 
     #region Quack Mono Behaviour
 
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+
+        // Initialize Facebook
+        if (!FB.IsInitialized)
+        {
+            // Initialize the Facebook SDK
+            FB.Init(initCallback, onHideUnity);
+        }
+        else
+        {
+            // Already initialized, signal an app activation App Event
+            FB.ActivateApp();
+        }
+    }
+
     protected override void OnStart()
     {
         activateServices();
@@ -91,6 +115,11 @@ public class RegistrationManager : QuackMonoBehaviour
             case eRegistrationMethodType.Google:
                 {
                     signInGoogle();
+                    break;
+                }
+            case eRegistrationMethodType.Facebook:
+                {
+                    signInFacebook();
                     break;
                 }
             default:
@@ -127,6 +156,63 @@ public class RegistrationManager : QuackMonoBehaviour
 
     #region Private Methods
 
+    #region Faecbook
+
+    private void initCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        }
+        else
+        {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+    private void onHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else
+        {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+
+    private void signInFacebook()
+    {
+        FB.LogInWithReadPermissions(FAECBOOK_READ_PERMISSIONS, authCallback);
+    }
+
+
+    private void authCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            // AccessToken class will have session details
+            var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+            // Print current access token's User ID
+            Debug.Log(aToken.UserId);
+            // Print current access token's granted permissions
+            foreach (string perm in aToken.Permissions)
+            {
+                Debug.Log(perm);
+            }
+        }
+        else
+        {
+            Debug.Log("User cancelled login");
+        }
+    }
+    #endregion
     private void activateServices()
     {
         PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
@@ -208,6 +294,7 @@ public class RegistrationManager : QuackMonoBehaviour
         });
     }
 
+    
     private void handleSignInUserEvent(bool result)
     {
         if (OnSignInEvent != null)
@@ -217,10 +304,10 @@ public class RegistrationManager : QuackMonoBehaviour
         }
     }
 
-    private UserRequestDelegate HandleSinInUserEvent()
-    {
-        throw new NotImplementedException();
-    }
+    //private UserRequestDelegate HandleSinInUserEvent()
+    //{
+    //    throw new NotImplementedException();
+    //}
 
     private void signOutGoogle()
     {
@@ -238,6 +325,7 @@ public class RegistrationManager : QuackMonoBehaviour
 public enum eRegistrationMethodType
 {
     Google,
+    Facebook,
 }
 
 public enum eRegistrationResultType
